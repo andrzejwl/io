db2d2f2d47977f51e37fcecf4ee5703dda5aa73e
Andrzej <andrzejw239@gmail.com>
2020-11-28 19:11:26 +0100

    add root file

---
+++ processing.py
@@ -0,0 +1,10 @@
+import cv2
+
+vidcap = cv2.VideoCapture('grupaA1.mp4')
+success,image = vidcap.read()
+count = 0
+
+while success:
+  success,image = vidcap.read()
+  print('Read a new frame: ', success, end='\r')
+  count += 1
