399d7c377321d18496415aa4a62125a6bc374f26
adambaranowski <adamb1066@gmail.com>
2020-12-05 23:01:48 +0100

    Writing to file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import io
 
 import detect_chars
 import detect_plates
@@ -23,30 +24,37 @@ def main():
         print("\nerror: KNN traning was not successful\n")
         return
 
-    #img_original_scene  = cv2.imread("resources/1.png")
-    vidcap = cv2.VideoCapture('resources/grupaA3.mp4')
+    # img_original_scene  = cv2.imread("resources/1.png")
+    vidcap = cv2.VideoCapture('resources/grupaA1.mp4')
     success, img_original_scene = vidcap.read()
 
+    detected_plates_txt = []
+
+    counter = 0
     while success:
-        if img_original_scene is None:
-            print("\nerror: image not read from file \n\n")
-            os.system("pause")
-            return
+        if counter % 10 == 0:
+            if img_original_scene is None:
+                print("\nerror: image not read from file \n\n")
+                os.system("pause")
+                return
 
-        list_of_possible_plates = detect_plates.detectPlatesInScene(img_original_scene) # detect plates
+            list_of_possible_plates = detect_plates.detectPlatesInScene(img_original_scene)  # detect plates
 
-        plates_to_check = list_of_possible_plates[:3]
+            plates_to_check = list_of_possible_plates[:3]
 
-        possible_plates = []
-        for p in plates_to_check:
-            text = pytesseract.image_to_string(p.imgPlate)
-            if len(text) > 3:
-                possible_plates.append(text)
+            possible_plates = []
+            for p in plates_to_check:
+                text = pytesseract.image_to_string(p.imgPlate)
+                if len(text) > 5:
+                    possible_plates.append(text)
 
-        if len(possible_plates):
-            print(possible_plates)
+            if len(possible_plates):
+                for plate in possible_plates:
+                    detected_plates_txt.append(plate)
+                print(possible_plates)
 
-        success, img_original_scene = vidcap.read()
+            success, img_original_scene = vidcap.read()
+        counter = counter + 1
         # OLD DETECTION METHOD
         # list_of_possible_plates = detect_chars.detect_chars_in_plates(list_of_possible_plates) # detect chars in plates
 
@@ -76,18 +84,27 @@ def main():
         #
         #     writeLicensePlateCharsOnImage(img_original_scene, lic_plate)
 
-            #success, img_original_scene = vidcap.read()
-            # cv2.imshow("img_original_scene", img_original_scene)
+        # success, img_original_scene = vidcap.read()
+        # cv2.imshow("img_original_scene", img_original_scene)
 
-            # cv2.imwrite("img_original_scene.png", img_original_scene)
+        # cv2.imwrite("img_original_scene.png", img_original_scene)
 
-    #cv2.waitKey(0)
+    # cv2.waitKey(0)
 
+    try:
+        f = io.open("license_plates.txt", "x", encoding="utf-8")
+    except:
+        print("File exist")
+
+    f = io.open("license_plates.txt", "w", encoding="utf-8")
+    for plate in detected_plates_txt:
+        f.write(plate)
+        f.write("\n")
+    f.close()
     return
 
 
 def drawRedRectangleAroundPlate(imgOriginalScene, licPlate):
-
     p2f_rect_points = cv2.boxPoints(licPlate.rrLocationOfPlateInScene)  # get 4 vertices of rotated rect
 
     # draw 4 red lines
@@ -101,37 +118,44 @@ def writeLicensePlateCharsOnImage(imgOriginalScene, licPlate):
     pt_center_of_text_area_x = 0  # this will be the center of the area the text will be written to
     pt_center_of_text_area_y = 0
 
-    pt_lower_left_text_origin_x = 0   # this will be the bottom left of the area that the text will be written to
+    pt_lower_left_text_origin_x = 0  # this will be the bottom left of the area that the text will be written to
     pt_lower_left_text_origin_y = 0
 
     scene_height, scene_width, scene_num_channels = imgOriginalScene.shape
     plate_height, plate_width, plate_num_channels = licPlate.imgPlate.shape
 
-    int_font_face = cv2.FONT_HERSHEY_SIMPLEX                      # choose a plain jane font
-    flt_font_scale = float(plate_height) / 30.0                    # base font scale on height of plate area
-    int_font_thickness = int(round(flt_font_scale * 1.5))           # base font thickness on font scale
+    int_font_face = cv2.FONT_HERSHEY_SIMPLEX  # choose a plain jane font
+    flt_font_scale = float(plate_height) / 30.0  # base font scale on height of plate area
+    int_font_thickness = int(round(flt_font_scale * 1.5))  # base font thickness on font scale
 
     text_size, baseline = cv2.getTextSize(licPlate.strChars, int_font_face, flt_font_scale, int_font_thickness)
 
     # unpack roatated rect into center point, width and height, and angle
-    ( (int_plate_center_x, int_plate_center_y), (int_plate_width, int_plate_height), flt_correction_angle_in_deg ) = licPlate.rrLocationOfPlateInScene
+    ((int_plate_center_x, int_plate_center_y), (int_plate_width, int_plate_height),
+     flt_correction_angle_in_deg) = licPlate.rrLocationOfPlateInScene
 
     int_plate_center_x = int(int_plate_center_x)  # make sure center is an integer
     int_plate_center_y = int(int_plate_center_y)
 
-    pt_center_of_text_area_x = int(int_plate_center_x)  # the horizontal location of the text area is the same as the plate
+    pt_center_of_text_area_x = int(
+        int_plate_center_x)  # the horizontal location of the text area is the same as the plate
 
-    if int_plate_center_y < (scene_height * 0.75):    # if the license plate is in the upper 3/4 of the image
-        pt_center_of_text_area_y = int(round(int_plate_center_y)) + int(round(plate_height * 1.6))  # write the chars in below the plate
+    if int_plate_center_y < (scene_height * 0.75):  # if the license plate is in the upper 3/4 of the image
+        pt_center_of_text_area_y = int(round(int_plate_center_y)) + int(
+            round(plate_height * 1.6))  # write the chars in below the plate
     else:
-        pt_center_of_text_area_y = int(round(int_plate_center_y)) - int(round(plate_height * 1.6))  # write the chars in above the plate
+        pt_center_of_text_area_y = int(round(int_plate_center_y)) - int(
+            round(plate_height * 1.6))  # write the chars in above the plate
 
     text_size_width, text_size_height = text_size
 
-    pt_lower_left_text_origin_x = int(pt_center_of_text_area_x - (text_size_width / 2))  # calculate the lower left origin of the text area
-    pt_lower_left_text_origin_y = int(pt_center_of_text_area_y + (text_size_height / 2))  # based on the text area center, width, and height
+    pt_lower_left_text_origin_x = int(
+        pt_center_of_text_area_x - (text_size_width / 2))  # calculate the lower left origin of the text area
+    pt_lower_left_text_origin_y = int(
+        pt_center_of_text_area_y + (text_size_height / 2))  # based on the text area center, width, and height
 
-    cv2.putText(imgOriginalScene, licPlate.strChars, (pt_lower_left_text_origin_x, pt_lower_left_text_origin_y), int_font_face, flt_font_scale, SCALAR_YELLOW, int_font_thickness)
+    cv2.putText(imgOriginalScene, licPlate.strChars, (pt_lower_left_text_origin_x, pt_lower_left_text_origin_y),
+                int_font_face, flt_font_scale, SCALAR_YELLOW, int_font_thickness)
 
 
 if __name__ == "__main__":
